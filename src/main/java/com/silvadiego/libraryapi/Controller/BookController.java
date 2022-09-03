a288caffd5fc0b9d623b7ef40827c0f61e4b5eb0
diegobsailva10 <diegobdasilva12@gmail.com>
2022-09-02 22:47:56 -0300

    Refatorando o Controller

--- src/main/java/com/silvadiego/libraryapi/Controller/BookController.java
+++ src/main/java/com/silvadiego/libraryapi/Controller/BookController.java
@@ -1,6 +1,8 @@
 package com.silvadiego.libraryapi.Controller;
 
 import com.silvadiego.libraryapi.DTO.BookDTO;
+import com.silvadiego.libraryapi.Model.Book;
+import com.silvadiego.libraryapi.Service.BookService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,10 +10,30 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/books/")
 public class BookController {
 
+    private BookService service;
+
+    public BookController (BookService service) {
+        this.service = service;
+    }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public BookDTO create(@RequestBody BookDTO dto){
-        return dto;
+
+        Book entity = Book.builder()
+                .author(dto.getAuthor())
+                .title(dto.getTitle())
+                .isbn(dto.getIsbn())
+                .build();
+
+        entity = service.save(entity);
+
+        return BookDTO.builder()
+                .id(entity.getId())
+                .author(entity.getAuthor())
+                .title(entity.getTitle())
+                .isbn(entity.getIsbn())
+                .build();
 
     }
 }
