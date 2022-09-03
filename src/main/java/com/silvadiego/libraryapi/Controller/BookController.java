73a2a741d6d70532a26ecef1cc69ccaa0ce5cddc
diegobsailva10 <diegobdasilva12@gmail.com>
2022-09-03 16:13:29 -0300

    ExceptionHandler

--- src/main/java/com/silvadiego/libraryapi/Controller/BookController.java
+++ src/main/java/com/silvadiego/libraryapi/Controller/BookController.java
@@ -1,12 +1,19 @@
 package com.silvadiego.libraryapi.Controller;
 
 import com.silvadiego.libraryapi.DTO.BookDTO;
+import com.silvadiego.libraryapi.Exceptions.ApiErrors;
 import com.silvadiego.libraryapi.Model.Book;
 import com.silvadiego.libraryapi.Service.BookService;
+import org.hibernate.type.ObjectType;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
-import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/books/")
@@ -22,12 +29,19 @@ public class BookController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public BookDTO create(@RequestBody BookDTO dto){
+    public BookDTO create(@RequestBody @Valid BookDTO dto){
 
         Book entity = modelMapper.map(dto, Book.class);
         entity = service.save(entity);
 
         return modelMapper.map(entity, BookDTO.class);
 
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleValidationExceptions(MethodArgumentNotValidException exception){
+        BindingResult bindingResult =  exception.getBindingResult();
+       return new ApiErrors(bindingResult);
     }
 }
