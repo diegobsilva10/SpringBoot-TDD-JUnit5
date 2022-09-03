6ab80752e768e8f055d90243b71eb2d53129987a
diegobsailva10 <diegobdasilva12@gmail.com>
2022-09-02 23:15:58 -0300

    Refatorando o controller com o ModelMapper

--- src/main/java/com/silvadiego/libraryapi/Controller/BookController.java
+++ src/main/java/com/silvadiego/libraryapi/Controller/BookController.java
@@ -3,7 +3,9 @@ package com.silvadiego.libraryapi.Controller;
 import com.silvadiego.libraryapi.DTO.BookDTO;
 import com.silvadiego.libraryapi.Model.Book;
 import com.silvadiego.libraryapi.Service.BookService;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,29 +13,21 @@ import org.springframework.web.bind.annotation.*;
 public class BookController {
 
     private BookService service;
+    private ModelMapper modelMapper;
 
-    public BookController (BookService service) {
+    public BookController (BookService service, ModelMapper mapper) {
         this.service = service;
+        this.modelMapper = mapper;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public BookDTO create(@RequestBody BookDTO dto){
 
-        Book entity = Book.builder()
-                .author(dto.getAuthor())
-                .title(dto.getTitle())
-                .isbn(dto.getIsbn())
-                .build();
-
+        Book entity = modelMapper.map(dto, Book.class);
         entity = service.save(entity);
 
-        return BookDTO.builder()
-                .id(entity.getId())
-                .author(entity.getAuthor())
-                .title(entity.getTitle())
-                .isbn(entity.getIsbn())
-                .build();
+        return modelMapper.map(entity, BookDTO.class);
 
     }
 }
